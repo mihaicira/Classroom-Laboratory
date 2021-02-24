ed4837309dfdeafd381a48669bf21d35ef6ae508
mihaicira <ciramihai291@gmail.com>
2021-02-24 19:05:44 +0200

    double page solved (2)

--- js/effects.js
+++ js/effects.js
@@ -26,33 +26,3 @@ if(url.includes("Activities")){
     }
 }
 
-=======
-function redirect(link){
-    window.open(link);
-}
-url = window.location.href
-if(url.includes("Activities")){
-    if(url.includes("activity_")){
-        var id = "#activity_"+url[url.indexOf("activity_")+"activity_".length]
-        $(id).css({
-            border: "5px solid rgb(255,0,22)"
-        });
-        setTimeout(function(){
-            $(id).css({
-                border: "5px solid transparent"
-            });
-            setTimeout(function(){
-                $(id).css({
-                    border: "5px solid rgb(255,0,22)"
-                });
-                setTimeout(function(){
-                    $(id).css({
-                        border: "5px solid transparent"
-                    });
-                },500)
-            },500)
-        }, 500);
-    }
-}
-
->>>>>>> 194054ba3130774d9b836bede67b9737fb36b809
